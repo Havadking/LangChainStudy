b8a61dd9a5a0b9cc94369eafed0498e77e1b3e03
wangzhenxiao <2022613398@qq.com>
2024-12-16 15:39:36 +0800

    完成内存记忆的功能

--- src/study/langgraph/quick_start.py
+++ src/study/langgraph/quick_start.py
@@ -2,13 +2,14 @@ from typing import Annotated
 import os
 
 from langchain_community.tools import TavilySearchResults
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
 from typing_extensions import TypedDict
 
 from src.model.ZhipuAILLM import ZhipuAILLM
-from src.study.langgraph.BasicToolNode import BasicToolNode
 
 os.environ["TAVILY_API_KEY"] = "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"
 
@@ -55,27 +56,26 @@ llm = ZhipuAILLM()
 llm_with_tools = llm.bind_tools(tools=tools)
 
 
-## 添加tool_node
-tool_node = BasicToolNode(tools=tools)
-graph_builder.add_node("tools", tool_node)
+# 创建一个检查点(这里为了学习使用内存的存储，用于生产的时候可以sqlite存储)
+memory = MemorySaver()
 
 
-def route_tools(state: State):
-    """
-    Use in the conditional_edge to route to the ToolNode if the last message
-    has tool calls. Otherwise, route to the end.
-    """
-    if isinstance(state, list):
-        ai_message = state[-1]
-    elif messages := state.get("messages", []):
-        ai_message = messages[-1]
-    else:
-        raise ValueError(f"No messages found in input state to tool_edge: {state}")
-
-    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
-        return "tools"
-
-    return END
+# def route_tools(state: State):
+#     """
+#     Use in the conditional_edge to route to the ToolNode if the last message
+#     has tool calls. Otherwise, route to the end.
+#     """
+#     if isinstance(state, list):
+#         ai_message = state[-1]
+#     elif messages := state.get("messages", []):
+#         ai_message = messages[-1]
+#     else:
+#         raise ValueError(f"No messages found in input state to tool_edge: {state}")
+#
+#     if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+#         return "tools"
+#
+#     return END
 
 
 def chatbot(state: State):
@@ -85,18 +85,26 @@ def chatbot(state: State):
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
+## 创建并添加tool_node
+# tool_node = BasicToolNode(tools=tools)
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
 
 # The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
 # it is fine directly responding. This conditional routing defines the main agent loop.
+# graph_builder.add_conditional_edges(
+#     "chatbot",
+#     route_tools,
+#     # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+#     # It defaults to the identity function, but if you
+#     # want to use a node named something else apart from "tools",
+#     # You can update the value of the dictionary to something else
+#     # e.g., "tools": "my_tools"
+# {"tools": "tools", END: END},
+# )
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
-    # It defaults to the identity function, but if you
-    # want to use a node named something else apart from "tools",
-    # You can update the value of the dictionary to something else
-    # e.g., "tools": "my_tools"
-{"tools": "tools", END: END},
+    tools_condition,
 )
 
 # Any time a tool is called, we return to the chatbot to decide the next step
@@ -104,7 +112,27 @@ graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
 # graph_builder.add_edge("chatbot", END)
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
+
+# 首先，选择一个线程作为本次对话的键
+config = {"configurable": {"thread_id": "1"}}
+
+# user_input = "Hi there! My name is Will."
+# # The config is the **second positional argument** to stream() or invoke()!
+#
+# events = graph.stream({"messages": [("user", user_input)]}, config=config, stream_mode="values")
+#
+# for event in events:
+#     event["messages"][-1].pretty_print()
+#
+# user_input = "Remember my name?"
+#
+# # The config is the **second positional argument** to stream() or invoke()!
+# events = graph.stream(
+#     {"messages": [("user", user_input)]}, config, stream_mode="values"
+# )
+# for event in events:
+#     event["messages"][-1].pretty_print()
 
 # 打印看看我们构建的图
 # png = graph.get_graph().draw_mermaid()
@@ -112,7 +140,10 @@ graph = graph_builder.compile()
 
 
 def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
+    for event in graph.stream(
+            input={"messages": [("user", user_input)]},
+            config=config,
+    ):
         # print(event)
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
